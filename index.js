58c0316c521614fa03984bf5e38e8b43726ff2bd
komo556 <yuu6shima@gmail.com>
2026-04-17 19:29:37 +0900

    Initialize Discord bot client in index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("起動成功");
+});
+
+client.login(process.env.TOKEN);
